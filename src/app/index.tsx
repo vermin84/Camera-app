632c40074b2b802b7594230755c68fe06f559392
Vermin84 <verminator84@gmail.com>
2026-07-01 12:39:47 +0300

    some settings more

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,23 @@
 import { COLORS } from "@/constants/colors";
 import { useCameraPermissions, useMicrophonePermissions } from "expo-camera";
-import * as MediaLibrary from "expo-media-library";
 import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Button, StyleSheet, View } from "react-native";
 export default function HomeScreen() {
   const router = useRouter();
   const [permissions] = useCameraPermissions();
   const [micPermission] = useMicrophonePermissions();
-  const [status, setStatus] = useState<MediaLibrary.PermissionResponse | null>(null);
+  
+  
   useEffect(() => {
-  (async () => {
-    const res  = await MediaLibrary.requestPermissionsAsync();
-
-    setStatus(res);
-  })();
-}, []);
-  console.log(status)
-  useEffect(() => {
-    if (!permissions || !micPermission || !status) return;
-    if (!permissions?.granted || !micPermission.granted || !status?.granted) {
+    if (!permissions || !micPermission) return;
+    if (!permissions?.granted || !micPermission.granted) {
       router.replace("/permissionsScreen");
     }
-    if (permissions?.granted && micPermission.granted && status?.granted) {
+    if (permissions?.granted && micPermission.granted) {
       router.replace("/camera");
     }
-  }, [router, permissions, micPermission, status]);
+  }, [router, permissions, micPermission]);
   return (
     <View style={styles.wrapper}>
       <Button
--- src/app/permissionsScreen.tsx
+++ src/app/permissionsScreen.tsx
@@ -3,7 +3,6 @@ import PermissionBlock from "@/components/permissionScreen/PermissionBlock";
 import { COLORS } from "@/constants/colors";
 import { AntDesign } from "@react-native-vector-icons/ant-design";
 import { useCameraPermissions, useMicrophonePermissions } from "expo-camera";
-import * as MediaLibrary from "expo-media-library";
 import { useRouter } from "expo-router";
 import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -23,10 +22,10 @@ async function requestMicPermissions(){
 console.log(micPermissions, cameraPermissions)*/
   const [permissions, requestCameraPermissionsAsync] = useCameraPermissions();
   const [micPermission, requestMicrophonePermissionsAsync] = useMicrophonePermissions();
-  const [status, requestMediaLibraryPermissionsAsync] = MediaLibrary.usePermissions();
+
   const router = useRouter();
-  const isEnable = permissions?.granted === true && micPermission?.granted === true && status?.granted === true;
-  console.log(permissions, micPermission, status);
+  const isEnable = permissions?.granted === true && micPermission?.granted === true ;
+  console.log(permissions, micPermission);
   function nextScreen() {
     if (!isEnable) {
       Alert.alert(
@@ -68,18 +67,7 @@ console.log(micPermissions, cameraPermissions)*/
             onClick={requestMicrophonePermissionsAsync}
           />
         </View>
-        <View style={styles.permissionWrapper}>
-          <AntDesign
-            size={ICON_SIZE}
-            color={status?.granted ? COLORS.primary : COLORS.muted}
-            name="folder"
-          />
-          <PermissionBlock
-            title="Media Library"
-            value={status?.granted}
-            onClick={requestMediaLibraryPermissionsAsync}
-          />
-        </View>
+        
         <NextButton onPress={nextScreen} />
       </ScrollView>
     </SafeAreaView>
